ca7bdcad882d9beca7d0c3a0482097e077e197d6
Afiq雪 <themagicice25@gmail.com>
2026-01-20 15:17:10 +0800

    third commit

--- W2/play.php
+++ W2/play.php
@@ -2,4 +2,7 @@
 $name = "Afiq";
 echo "Hello World $name";
 echo "This is a full stack class".$name;
+echo "<h1>Nama saya Nur Afiq Fathurrahman</h1>";
+echo "<h2> is from JTMK<h2>";
+echo "<h3> and my PA is Miss Athirah</h3>";
 ?>
