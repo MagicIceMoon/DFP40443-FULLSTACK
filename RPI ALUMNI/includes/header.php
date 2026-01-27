9c0ccb50b3fcf0a68368a0bfad61ac67dd07b184
Afiq雪 <themagicice25@gmail.com>
2026-01-27 15:51:05 +0800

    commit homework RPI Alumni

--- RPI ALUMNI/includes/header.php
+++ RPI ALUMNI/includes/header.php
@@ -6,9 +6,14 @@
 <body>
     <header style="background-color: #f30000; padding: 10px 20px;">
         <h1 style="color: #ffffff;">&#127891; RPI Alumni</h1>
-        <nav>
-            <a href="">Evenet</a>
+        <nav class="mainnav">
+            <a href="">Event</a>
             <a href="">Chapters & Groups</a>
+            <a href="">Career Networking</a>
+            <a href="">Products & Benefits</a>
+            <a href="">Volunteer</a>
+            <a href="">Reunion & Homecoming</a>
+            <a href="">Give</a>
         </nav>
     </header>
 <hr>
