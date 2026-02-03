3ead5a311a29ffcd8dc299f9623fb0f9f3a0bae6
Afiq雪 <themagicice25@gmail.com>
2026-02-03 16:12:23 +0800

    commit week 4

--- W4/PAMU/index.php
+++ W4/PAMU/index.php
@@ -29,6 +29,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     </header>
     Welcome <?php echo $_POST['username']; ?>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan id ex in finibus. Donec viverra lorem at libero venenatis vestibulum. Etiam purus quam, feugiat eget dolor sit amet, pretium accumsan dui. Donec mi velit, tincidunt a nulla ut, congue tristique odio. Aenean ac augue turpis. Quisque venenatis eros vitae luctus imperdiet. In placerat, ligula quis finibus vehicula, risus nulla dapibus nisi, sit amet accumsan odio quam eu orci. Phasellus facilisis, magna sed interdum tempor, tortor odio commodo urna, at scelerisque tellus enim ut ante. Vestibulum non iaculis justo, sed pharetra purus. Morbi tincidunt nibh sed nisi vestibulum elementum. Curabitur at libero nec velit dapibus venenatis. Praesent tempor ligula sed felis condimentum viverra. Pellentesque in nunc in nisl venenatis tincidunt vel sit amet libero. Quisque scelerisque eleifend tristique. Phasellus elementum maximus lectus at efficitur.</p>
-    <footer></footer>
+    <hr>
+    <footer><?php echo $config['admin_site']?></footer>
 </body>
 </html>
