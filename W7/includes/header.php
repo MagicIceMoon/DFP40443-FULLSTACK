af43f3295f07afab1b121788f9d006e19b6aced8
Afiq雪 <themagicice25@gmail.com>
2026-02-26 12:32:15 +0800

    week 7

--- W7/includes/header.php
+++ W7/includes/header.php
@@ -1,15 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Portal</title>
+    <title>Dashboard</title>
 </head>
+<style>
+    .topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+</style>
 <body>
+        <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
     <header style="background-color: padding: 10px 20px;">
+        
         <h1>&#127891; Welcome</h1>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="grades.php">My Grades</a>
-            <a href="logout.php">Logout</a>
-        </nav>
     </header>
 <hr>
